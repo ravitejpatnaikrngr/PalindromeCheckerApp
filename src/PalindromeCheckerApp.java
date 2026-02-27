e8d727e5ae56148e9cfa9019f07845126f4e71e6
ravitejpatnaikrngr <rp7108@srmist.edu.in>
2026-02-27 10:50:30 +0530

    UC10

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,29 +1,38 @@
-
 import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("=================================");
-        System.out.println("Palindrome Checker App - UC9");
+        System.out.println("Palindrome Checker App - UC10");
         System.out.println("=================================");
         System.out.print("Enter a string to check: ");
         String input = scanner.nextLine();
-
-        if (isPalindromeRecursive(input, 0, input.length() - 1)) {
-            System.out.println("Result: The given string is a Palindrome.");
+        String normalized = normalizeString(input);
+        if (isPalindrome(normalized)) {
+            System.out.println("Result: The given string is a Palindrome (case & spaces ignored).");
         } else {
-            System.out.println("Result: The given string is NOT a Palindrome.");
+            System.out.println("Result: The given string is NOT a Palindrome (case & spaces ignored).");
         }
+
         scanner.close();
     }
-    public static boolean isPalindromeRecursive(String str, int start, int end) {
-        // Base condition: crossed pointers or single character
-        if (start >= end) {
-            return true;
+    public static String normalizeString(String str) {
+        str = str.toLowerCase();
+        str = str.replaceAll("[^a-z0-9]", "");
+
+        return str;
+    }
+    public static boolean isPalindrome(String str) {
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-        return isPalindromeRecursive(str, start + 1, end - 1);
+        return true;
     }
 }
