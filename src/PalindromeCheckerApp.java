2e41c448a262c157f846f0b4f93a5a4bba143d6e
ravitejpatnaikrngr <rp7108@srmist.edu.in>
2026-02-27 10:21:32 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,45 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
     private static final String APP_NAME = "Palindrome Checker App";
     private static final String VERSION = "1.0";
     public static void main(String[] args) {
-                String word = "madam";
-                System.out.println("Palindrome Checker App - UC5");
+                String word = "level";  // You can modify this
+                System.out.println("=================================");
+                System.out.println("Palindrome Checker App - UC6");
+                System.out.println("=================================");
                 System.out.println("Original String: " + word);
                 Stack<Character> stack = new Stack<>();
+                Queue<Character> queue = new LinkedList<>();
                 for (int i = 0; i < word.length(); i++) {
-                    stack.push(word.charAt(i));
+                    char ch = word.charAt(i);
+                    stack.push(ch);
+                    queue.add(ch);
                 }
-                String reversed = "";
+                boolean isPalindrome = true;
+
                 while (!stack.isEmpty()) {
-                    reversed += stack.pop();
+
+                    char fromStack = stack.pop();
+                    char fromQueue = queue.remove();
+
+                    if (fromStack != fromQueue) {
+                        isPalindrome = false;
+                        break;
+                    }
                 }
-                if (word.equals(reversed)) {
+                if (isPalindrome) {
                     System.out.println("Result: The given string is a Palindrome.");
                 } else {
                     System.out.println("Result: The given string is NOT a Palindrome.");
                 }
+
                 System.out.println("Program execution completed.");
             }
         }
+
 
 
 
