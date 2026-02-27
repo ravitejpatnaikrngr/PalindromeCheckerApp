4aaa803e285890d93ffb2a3b0002131dd19fa9df
ravitejpatnaikrngr <rp7108@srmist.edu.in>
2026-02-27 10:11:13 +0530

    UC4

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,22 +3,29 @@ public class PalindromeCheckerApp {
     private static final String APP_NAME = "Palindrome Checker App";
     private static final String VERSION = "1.0";
     public static void main(String[] args) {
-                String word = "level";
-                System.out.println("=================================");
-                System.out.println("Palindrome Checker App - UC3");
-                System.out.println("=================================");
+                String word = "radar";
+                System.out.println("Palindrome Checker App - UC4");
                 System.out.println("Original String: " + word);
-                String reversed = "";
-                for (int i = word.length() - 1; i >= 0; i--) {
-                    reversed = reversed + word.charAt(i);
+                char[] characters = word.toCharArray();
+                int start = 0;
+                int end = characters.length - 1;
+                boolean isPalindrome = true;
+                while (start < end) {
+                    if (characters[start] != characters[end]) {
+                        isPalindrome = false;
+                        break;
+                    }
+                    start++;
+                    end--;
                 }
-                System.out.println("Reversed String: " + reversed);
-                if (word.equals(reversed)) {
+                if (isPalindrome) {
                     System.out.println("Result: The given string is a Palindrome.");
                 } else {
                     System.out.println("Result: The given string is NOT a Palindrome.");
                 }
+
                 System.out.println("Program execution completed.");
             }
         }
+
 
